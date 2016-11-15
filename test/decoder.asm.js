eac4095226034f28aeee52a11a57cad9edc1a316
Friedel Ziegelmayer <dignifiedquire@gmail.com>
2016-11-15 12:38:59 +0100

    uints passing

--- test/decoder.asm.js
+++ test/decoder.asm.js
@@ -3,15 +3,36 @@
 const cases = require('./cases')
 const asm = require('../lib/decoder.asm')
 const assert = require('assert')
+const bignumber = require('bignumber.js')
+const SHIFT16 = Math.pow(2, 16)
+const SHIFT32 = Math.pow(2, 32)
+const MAX_SAFE_HIGH = 0x1fffff
 
 const buffer = new ArrayBuffer(0x10000)
 const buffer8 = new Uint8Array(buffer)
 
 let res = []
 
+function buildInt32 (f, g) {
+  return f * SHIFT16 + g
+}
+
 const foreign = {
   pushInt (val) {
     res.push(val)
+  },
+  pushInt32 (f, g) {
+    res.push(buildInt32(f, g))
+  },
+  pushInt64 (f1, f2, g1, g2) {
+    const f = buildInt32(f1, f2)
+    const g = buildInt32(g1, g2)
+    console.log('int64', f, g)
+    if (f > MAX_SAFE_HIGH) {
+      res.push(new bignumber(f).times(SHIFT32).plus(g))
+    } else {
+      res.push((f * SHIFT32) + g)
+    }
   },
   log (val, val2) {
     console.log('--', val, val2)
