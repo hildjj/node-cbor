e1bcf766e9250179bb76cfb7a035b77653dafce2
Friedel Ziegelmayer <dignifiedquire@gmail.com>
2016-11-22 22:05:37 +0100

    test and cleanup utils

--- src/utils.js
+++ src/utils.js
@@ -1,36 +1,11 @@
 'use strict'
 
-const fs = require('fs')
-const stream = require('stream')
 const Bignumber = require('bignumber.js')
 
 const constants = require('./constants')
-const NUMBYTES = constants.NUMBYTES
 const SHIFT32 = constants.SHIFT32
 const SHIFT16 = constants.SHIFT16
 const MAX_SAFE_HIGH = 0x1fffff
-
-exports.parseCBORint = function (ai, buf) {
-  var f, g
-  switch (ai) {
-    case NUMBYTES.ONE:
-      return buf.readUInt8(0, true)
-    case NUMBYTES.TWO:
-      return buf.readUInt16BE(0, true)
-    case NUMBYTES.FOUR:
-      return buf.readUInt32BE(0, true)
-    case NUMBYTES.EIGHT:
-      f = buf.readUInt32BE(0)
-      g = buf.readUInt32BE(4)
-      if (f > MAX_SAFE_HIGH) {
-        return new Bignumber(f).times(SHIFT32).plus(g)
-      } else {
-        return (f * SHIFT32) + g
-      }
-    default:
-      throw new Error('Invalid additional info for int: ' + ai)
-  }
-}
 
 exports.parseHalf = function parseHalf (buf) {
   var exp, mant, sign
@@ -44,77 +19,6 @@ exports.parseHalf = function parseHalf (buf) {
   } else {
     return sign * Math.pow(2, exp - 25) * (1024 + mant)
   }
-}
-
-exports.parseCBORfloat = function (buf) {
-  switch (buf.length) {
-    case 2:
-      return exports.parseHalf(buf)
-    case 4:
-      return buf.readFloatBE(0, true)
-    case 8:
-      return buf.readDoubleBE(0, true)
-    default:
-      throw new Error('Invalid float size: ' + buf.length)
-  }
-}
-
-exports.hex = function (s) {
-  return new Buffer(s.replace(/^0x/, ''), 'hex')
-}
-
-exports.bin = function (s) {
-  var chunks, end, start
-  s = s.replace(/\s/g, '')
-  start = 0
-  end = (s.length % 8) || 8
-  chunks = []
-  while (end <= s.length) {
-    chunks.push(parseInt(s.slice(start, end), 2))
-    start = end
-    end += 8
-  }
-  return new Buffer(chunks)
-}
-
-exports.arrayEqual = function (a, b) {
-  if ((a == null) && (b == null)) {
-    return true
-  }
-  if ((a == null) || (b == null)) {
-    return false
-  }
-  return (a.length === b.length) && a.every(function (elem, i) {
-    return elem === b[i]
-  })
-}
-
-exports.bufferEqual = function (a, b) {
-  var byte
-  var i
-  var j
-  var len
-  var ret
-
-  if ((a == null) && (b == null)) {
-    return true
-  }
-  if ((a == null) || (b == null)) {
-    return false
-  }
-  if (!(Buffer.isBuffer(a) && Buffer.isBuffer(b) && (a.length === b.length))) {
-    return false
-  }
-  ret = true
-  for (i = j = 0, len = a.length; j < len; i = ++j) {
-    byte = a[i]
-    ret &= b[i] === byte
-  }
-  return Boolean(ret)
-}
-
-exports.bufferToBignumber = function (buf) {
-  return new Bignumber(buf.toString('hex'), 16)
 }
 
 function toHex (n) {
@@ -133,59 +37,6 @@ exports.arrayBufferToBignumber = function (buf) {
   }
 
   return new Bignumber(res, 16)
-}
-
-exports.DeHexStream = class DeHexStream extends stream.Readable {
-  constructor (hex) {
-    super()
-    hex = hex.replace(/^0x/, '')
-    if (hex) {
-      this.push(new Buffer(hex, 'hex'))
-    }
-    this.push(null)
-  }
-}
-
-exports.HexStream = class HexStream extends stream.Transform {
-  _transform (fresh, encoding, cb) {
-    this.push(fresh.toString('hex'))
-    return cb()
-  }
-}
-
-function printError (er) {
-  if (er != null) {
-    return console.log(er)
-  }
-}
-
-exports.streamFiles = function (files, streamFunc, cb) {
-  if (cb == null) {
-    cb = printError
-  }
-  const f = files.shift()
-  if (!f) {
-    return cb()
-  }
-  const sf = streamFunc()
-  sf.on('end', function () {
-    return exports.streamFiles(files, streamFunc, cb)
-  })
-  sf.on('error', cb)
-  const s = (f === '-') ? process.stdin : (f instanceof stream.Stream) ? f : fs.createReadStream(f)
-  s.on('error', cb)
-  return s.pipe(sf)
-}
-
-exports.guessEncoding = function (input) {
-  switch (false) {
-    case typeof input !== 'string':
-      return 'hex'
-    case !Buffer.isBuffer(input):
-      return undefined
-    default:
-      throw new Error('Unknown input type')
-  }
 }
 
 // convert an Object into a Map
---
+++ test/utils.spec.js
@@ -0,0 +1,66 @@
+/* eslint-env mocha */
+'use strict'
+
+const expect = require('chai').expect
+const utils = require('../src/utils')
+
+function bin (s) {
+  let chunks
+  let end
+  let start
+
+  s = s.replace(/\s/g, '')
+  start = 0
+  end = (s.length % 8) || 8
+  chunks = []
+  while (end <= s.length) {
+    chunks.push(parseInt(s.slice(start, end), 2))
+    start = end
+    end += 8
+  }
+
+  return new Buffer(chunks)
+}
+
+describe('utils', () => {
+  it('parseHalf', () => {
+    expect(
+      utils.parseHalf(bin('0011110000000000'))
+    ).to.be.eql(1)
+    expect(
+      utils.parseHalf(bin('1100000000000000'))
+    ).to.be.eql(-2)
+    expect(
+      utils.parseHalf(bin('0111101111111111'))
+    ).to.be.eql(65504)
+    expect(
+      utils.parseHalf(bin('0000010000000000'))
+    ).to.be.eql(0.00006103515625)
+    expect(
+      utils.parseHalf(bin('0000000000000000'))
+    ).to.be.eql(0)
+    expect(
+      utils.parseHalf(bin('1000000000000000'))
+    ).to.be.eql(-0)
+    expect(
+      utils.parseHalf(bin('0111110000000000'))
+    ).to.be.eql(Infinity)
+    expect(
+      utils.parseHalf(bin('1111110000000000'))
+    ).to.be.eql(-Infinity)
+  })
+
+  it('buildMap', () => {
+    expect(
+      Array.from(utils.buildMap({
+        hello: 'world',
+        1: 2,
+        '/$)§)': 89
+      }))
+    ).to.be.eql([
+      ['1', 2],
+      ['hello', 'world'],
+      ['/$)§)', 89]
+    ])
+  })
+})
