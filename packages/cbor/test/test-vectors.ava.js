85016ff5fbcd28de0a72d851bb00dcf1a6c5491a
Joe Hildebrand <joe-github@cursive.net>
2022-07-08 23:34:39 -0600

    Update dependencies

--- packages/cbor/test/test-vectors.ava.js
+++ packages/cbor/test/test-vectors.ava.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 'use strict'
 const cbor = require(process.env.CBOR_PACKAGE || '../')
 const test = require('ava')
--- packages/plain-demo/build.js
+++ packages/plain-demo/build.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 'use strict'
 
 const fs = require('fs')
