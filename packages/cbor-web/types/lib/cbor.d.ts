bb3787a11f7ad6d8508574b91e293be825765430
rouzwelt <rouzweltt@gmail.com>
2023-03-22 21:00:49 +0000

    include types for cbor-web

---
+++ packages/cbor-web/types/lib/cbor.d.ts
@@ -0,0 +1,25 @@
+export const Commented: typeof import("./commented");
+export const Diagnose: typeof import("./diagnose");
+export const Decoder: typeof import("./decoder");
+export const Encoder: typeof import("./encoder");
+export const Simple: typeof import("./simple");
+export const Tagged: typeof import("./tagged");
+export const Map: typeof import("./map");
+export namespace leveldb {
+    const decode: typeof import("./decoder").decodeFirstSync;
+    const encode: typeof import("./encoder").encode;
+    const buffer: boolean;
+    const name: string;
+}
+export function reset(): void;
+export const comment: typeof import("./commented").comment;
+export const decodeAll: typeof import("./decoder").decodeAll;
+export const decodeAllSync: typeof import("./decoder").decodeAllSync;
+export const decodeFirst: typeof import("./decoder").decodeFirst;
+export const decodeFirstSync: typeof import("./decoder").decodeFirstSync;
+export const decode: typeof import("./decoder").decodeFirstSync;
+export const diagnose: typeof import("./diagnose").diagnose;
+export const encode: typeof import("./encoder").encode;
+export const encodeCanonical: typeof import("./encoder").encodeCanonical;
+export const encodeOne: typeof import("./encoder").encodeOne;
+export const encodeAsync: typeof import("./encoder").encodeAsync;
